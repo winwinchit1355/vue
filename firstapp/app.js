9c63d9d5b0fc97bb03683745d6e4dbb6b4e0ad17
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-03-02 23:13:42 +0630

    for loop computed properties

--- firstapp/app.js
+++ firstapp/app.js
@@ -3,9 +3,17 @@ const app = Vue.createApp({
     data(){
         return {
             showBook:true,
+            url: 'https://www.google.com/',
+            books:[
+                {title:'name of the wind',author:'patrick rock',feature_image:'assets/images/book1.jpeg',is_favorite:true},
+                {title:'the way of king',author:'JOhin',feature_image:'assets/images/book2.jpeg',is_favorite:false},
+                {title:'book3',author:'author3',feature_image:'assets/images/book3.jpeg',is_favorite:false},
+            ],
             title:'The final empire',
             author:'Brandon',
-            age:42
+            age:42,
+            x:0,
+            y:0
         }
     },
     methods:{
@@ -16,8 +24,23 @@ const app = Vue.createApp({
         toggleShowBooks(){
             this.showBook = !this.showBook;
         },
-        handleEvent(e){
-            console.log(e)
+        handleEvent(e,data){
+            console.log(e,e.type);
+            if(data){
+                console.log(data);
+            }
+        },
+        handleMouseMove(e){
+            this.x=e.offsetX;
+            this.y=e.offsetY;
+        },
+        toggleFav(book){
+            book.is_favorite = ! book.is_favorite;
+        }
+    },
+    computed:{
+        filterBooks(){
+            return this.books.filter((book)=>book.is_favorite)
         }
     }
 });
