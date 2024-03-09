d7ee739e779081a636dd2cc9231ffa5acbff8445
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-03-09 15:03:00 +0630

    fix

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -59,7 +59,8 @@ export default {
 </script>
 
 <style>
-#app,.modals {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
