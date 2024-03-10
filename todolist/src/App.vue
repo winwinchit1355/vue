deb427334b841a68fef5865c9aed4b27c010944c
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-03-10 23:40:32 +0630

    create todo app

---
+++ todolist/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <h1>{{title}}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      title: 'My To Do App',
+      tasks:[
+        {id:1,name:'Learn Vue',finished:false},
+        {id:2,name:'Learn PHP',finished:false},
+        {id:3,name:'Learn English',finished:true},
+      ]
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
