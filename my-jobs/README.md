8c0d1a4ff024cf28a66fc07e402b83c386f35f2c
winwinchit1355 <winwinchit1355@gmail.com>
2024-03-18 16:53:15 +0630

    vue router

---
+++ my-jobs/README.md
@@ -0,0 +1,28 @@
+# my-jobs
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### Reference Link
+https://www.youtube.com/watch?v=juocv4AtrHo&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=8
---
+++ my-jobs/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="nav">
+    <RouterLink :to="{name:'Home'}">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#nav{
+  padding: 30px;
+  box-sizing: border-box;
+}
+#nav a{
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 0 10px 0 0;
+  padding: 0 10px 0 0;
+}
+#nav a:not(:last-child){
+  border-right: 1px solid black;
+}
+#nav a.router-link-exact-active{
+  color: #42b983;
+}
+</style>
