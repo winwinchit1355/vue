f0cfc134d57e273c06e803560792feb151724d50
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-04-01 21:38:03 +0630

    ref in composition api

--- dojo-blog/src/App.vue
+++ dojo-blog/src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="home">
-    <p>My name is {{ name }}</p>
-    <p>I am {{ age }} years old.</p>
+    <p ref="p">My name is {{ name }}. I am {{ age }} years old.</p>
     <button @click="handleClick">Click Me</button>
   </div>
 </template>
 <script>
+import { ref } from 'vue';
 export default{
   name:'Home',
   setup(){
     console.log('setup');
     let name="WWC";
     let age=30;
+    const p = ref(null);
+    console.log(p,p.value);
 
     const handleClick = () =>{
-      console.log('click me');
+      console.log(p,p.value);
+      p.value.classList.add('test');
+      p.value.textContent = 'hello world';
     }
-    return {name,age,handleClick};
+    return {name,age,handleClick,p};
   },
   data(){
     return {
