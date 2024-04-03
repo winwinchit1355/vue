5de79bb9d4d571a24e8fe57db748c09ee5a88f6f
winwinchit1355 <winwinchit1355@gmail.com>
2024-04-03 09:30:43 +0630

    test reactive

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -4,9 +4,10 @@
     <button @click="back">Go Back</button>
     <h2>Refs</h2>
     <p>{{ data.name }} - {{ data.age }}</p>
-    <button @click="updateData">Update</button>
+    <button @click="updateData">Update Data</button>
     <h2>Reactive</h2>
-    <p>{{ reactiveData.name }} - {{ reactiveData.age }}</p>
+    <p>{{ reactiveData.name }} - {{ reactiveData.age }} - {{ nameTest }}</p>
+    <button @click="updateReactive">Update Reactive</button>
   </div>
 </template>
 
@@ -17,10 +18,17 @@ export default {
   setup(){
     const data = ref({name:'wwc',age:22});
     const reactiveData = reactive({name:'ayage',age:34});
+
+    // test reactive with primitive value 
+    const nameTest = ref('aa');
     const updateData = () =>{
       data.value.age = 40;
     }
-    return {data,updateData,reactiveData};
+    const updateReactive = () =>{
+      reactiveData.age = 55;
+      nameTest.value = 'bb';
+    }
+    return {data,updateData,reactiveData,updateReactive};
   },
   methods:{
     back(){
